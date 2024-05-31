0def975341349b05d7c5c8d5c278c964da969b13
Maeinukhan <khanmaeinuddin19@gmail.com>
2024-05-31 16:57:07 +0530

    Java project 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+    RWOnly rw=new RWOnly();
+    // rw.name="John doe";  // ERROR: name has private access in RWOnly 
+    // System.out.println(rw.name);// ERROR : name has private access in RWOnly
+
+    rw.setValue("john doe");
+    System.out.println(rw.getValue());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getValue(){
+        return name;
+    }
+
+    public void setValue(String name){
+        this.name=name;
+    }
 }
